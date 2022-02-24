5d8f81c5c9f94e436df2d407aecf3e3c133569fb
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-02-25 00:02:02 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+</html>
+<head>
+      <title>   WebSeminar Reservation    </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo"> WebSeminar Reservation </h2>
+            </div>
+            <div class="menu">
+           	    <ul>
+           		    <li><a href="#">Seminar</a></li>
+           		    <li><a href="#">Webinar</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="form">
+        	<form action="" method="post">
+        		<div id="login">
+        		<h3>Login</h3><br>
+        		<input type="text" name="username" placeholder="Enter your username"><br><br>
+        		<input type="text" name="password" placeholder="Enter your password"><br><br>
+        		<button class="btn"></a>Login</button>
+        		</div>
+        	</form>
+        </div>
+        <div>
+            <p class="introduction">WebSeminar is a management platform to organize registration for Seminar Event and Webinar Event.</p>
+        </div>
+    </div>
+      
+       
+</body>
+</html>
