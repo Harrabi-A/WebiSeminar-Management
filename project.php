3d8173a64e88c6c54a6db3edba51cfb9af75a8df
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-02-25 10:24:58 +0100

    Add files via upload

---
+++ project.php
@@ -0,0 +1,29 @@
+<!DOCTYPE  html>
+
+<html>
+<head>
+	<title>Project</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="main">
+		<div class="navbar">
+            <div class="icon">
+                <h2 class="logo"> WebSeminar Reservation </h2>
+            </div>
+            <div class="menu">
+           	    <ul>
+                    <li><a href="seminar.php">Seminar</a></li>
+                    <li><a href="webinar.php">Webinar</a></li>
+                    <li><a href="project.php">Project</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </div>
+		<p id="project">WebSeminar platform is a university project for DataBase and Web COURSE <br> 
+			link to source code:  <a href="https://github.com/Harrabi-A/WebiSeminar-Management">Github repository</a></p>
+		
+
+	</div>
+</body>
+</html>
