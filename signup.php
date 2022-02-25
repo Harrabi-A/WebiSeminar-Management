3b6e784d21f7c2eb7b1d4df4be8de4c5e8f5ba8c
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-02-25 10:25:37 +0100

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+if (isset($_SESSION['username'])) {
+ 
+   // user is set
+	header("Location: index.php");
+ 
+
+ } else {
+   
+ }
+
+?>
+
+</html>
+<head>
+      <title>   WebSeminar Reservation    </title>
+      <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo"> WebSeminar Reservation </h2>
+            </div>
+            <div class="menu">
+           	    <ul>
+                    <li><a href="seminar.php">Seminar</a></li>
+                    <li><a href="webinar.php">Webinar</a></li>
+                    <li><a href="project.php">Project</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="form">
+        	<form action="" method="post">
+        		<div id="signup">
+        		<h3>Sign-up</h3><br>
+        		<input type="text" name="username" placeholder="Enter your email"><br><br>
+        		<input type="text" name="text" placeholder="Enter your password"><br><br>
+                <input type="text" name="text" placeholder="Enter your name"><br><br>
+                <input type="text" name="text" placeholder="Enter your surname"><br><br>
+        		<button class="btn"></a>Sign-up</button>
+        		</div>
+        	</form>
+        </div>
+        <div>
+            <p class="introduction">WebSeminar is a management platform to organize registration for Seminar Event and Webinar Event.</p>
+        </div>
+    </div>
+      
+       
+</body>
+</html>
