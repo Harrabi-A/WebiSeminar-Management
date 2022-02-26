43cb34703809e3e17252a895110316b5ca8fb90a
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-02-26 23:17:02 +0100

    Add files via upload

---
+++ participationWebinar.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+	
+
+$keys=array_keys($_POST);
+print_r($keys);
+
+$eventID=$keys['0'];
+$email=$_SESSION['user'];
+
+$user="root";
+$host="localhost";
+$password="";
+$database="WebSeminar";
+
+
+// Create connection
+$conn = mysqli_connect($host,$user,$password,$database);
+$query = "INSERT INTO PARTICIPATIONWEBINAR (email,id) VALUES ('$email','$eventID')";
+echo "$query";
+$result = mysqli_query($conn,$query,MYSQLI_STORE_RESULT);
+}
+header("Location: login.php");
+?>
