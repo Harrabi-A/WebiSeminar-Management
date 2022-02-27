d3d14b2e0515278cabc5383d502cb6f840b103bd
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-02-27 09:38:40 +0100

    Update removeSeminar.php

--- removeSeminar.php
+++ removeSeminar.php
@@ -12,17 +12,17 @@ $database="WebSeminar";
 
 // Establish connection
 $conn = mysqli_connect($host,$user,$password,$database);
-$query= "SELECT * FROM SEMINAR WHERE id='$eventID' AND manager='$email'"; // This Query is used to check if the user how request seminar remove is the manager 
+$query= "SELECT * FROM WEBINAR WHERE id='$eventID' AND manager='$email'"; // This Query is used to check if the user how request seminar remove is the manager 
 $result= mysqli_query($conn,$query);
 if ($rows=mysqli_fetch_assoc($result)) {
 	// the user is tthe manager
 	// delete seminar 
-	$query2="DELETE FROM SEMINAR WHERE id='$eventID'";
+	$query2="DELETE FROM WEBINAR WHERE id='$eventID'";
 	$result2 = mysqli_query($conn,$query2,MYSQLI_STORE_RESULT);
 	header("Location: home.php");
 }else{
 	// the user  is participant
-	$query3= "DELETE FROM PARTICIPATIONSEMINAR WHERE id='$eventID' AND email='$email'";
+	$query3= "DELETE FROM PARTICIPATIONWEBINAR WHERE id='$eventID' AND email='$email'";
 	echo "$query3";
 	$result3 = mysqli_query($conn,$query3,MYSQLI_STORE_RESULT);
 	header("Location: home.php");
