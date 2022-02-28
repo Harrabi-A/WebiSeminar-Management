213be47de4efc0fa68c461d8025b6e45c44ab1d1
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-02-28 09:54:01 +0100

    Add files via upload

---
+++ newuser.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+if (isset($_SESSION['user'])) {
+	header("Location: home.php");
+}
+if ($_POST['email'] && $_POST['password'] && $_POST['name'] && $_POST['surname']) {
+	# code...
+
+    $email = trim($_POST['email']);
+    $userpass = trim($_POST['password']);
+    $name = trim($_POST['name']);
+    $surname = trim($_POST['surname']);
+
+    $user="root";
+    $host="localhost";
+    $password="";
+    $database="WebSeminar";
+
+
+    // Create connection
+    $conn = mysqli_connect($host,$user,$password,$database);
+    $query = "INSERT INTO USER VALUES ('$email','$userpass','$name','surname')";
+    mysqli_query($conn,$query);
+
+    $query2 = "SELECT * FROM USER WHERE email='$email'";
+
+    $result = mysqli_query($conn,$query2);
+    if ($rows=mysqli_fetch_assoc($result)) {
+    	$_SESSION['user']=$email;
+    	header("Location: home.php");
+    }
+
+}
+?>
