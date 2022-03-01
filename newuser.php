a812d84583dc875d58d3965cfebeab8d64d155fc
Harrabi-A <64797470+Harrabi-A@users.noreply.github.com>
2022-03-02 00:05:07 +0100

    Update newuser.php

--- newuser.php
+++ newuser.php
@@ -15,18 +15,28 @@ if ($_POST['email'] && $_POST['password'] && $_POST['name'] && $_POST['surname']
     $password="";
     $database="WebSeminar";
 
-
     // Create connection
     $conn = mysqli_connect($host,$user,$password,$database);
+    
+
+    $query2 = "SELECT * FROM USER WHERE email='$email'";
+
+    //check if user already exist
+    $result = mysqli_query($conn,$query2);
+    if(isset($result)){
+        header("Location: signup.php");
+    }else{
+
     $query = "INSERT INTO USER VALUES ('$email','$userpass','$name','surname')";
     mysqli_query($conn,$query);
 
-    $query2 = "SELECT * FROM USER WHERE email='$email'";
+    
 
     $result = mysqli_query($conn,$query2);
     if ($rows=mysqli_fetch_assoc($result)) {
     	$_SESSION['user']=$email;
     	header("Location: home.php");
+    }
     }
 
 }else{
